8f79db0729649adc6c6d8f7dfafe98ae3ebc5c95
Jude Lawrence <jude.lawrence@kcl.ac.uk>
2022-12-09 14:19:49 +0000

    Solution

--- median.py
+++ median.py
@@ -1,14 +1,11 @@
-"""Median calculator."""
-"""ENTER YOUR SOLUTION HERE!"""
-
 while True:
     try:
-        print("Enter a list of numbers separated by commas: ")
+        print("Enter a list of numbers separated by commas.")
         numbers = [float(value) for value in input().split(",")]
         numbers.sort()
         length = len(numbers)
 
-        if (length % 2 == 1):
+        if length % 2 == 1:
             median = numbers[length // 2]
         else:
             median = (numbers[(length//2-1)] + numbers[length//2]) /2
