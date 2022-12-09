40d02e2c75c8794057ffa06b57d0402b2db52154
Jude Lawrence <jude.lawrence@kcl.ac.uk>
2022-12-09 14:13:44 +0000

    Solution

--- median.py
+++ median.py
@@ -5,6 +5,13 @@ while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+        numbers.sort()
+        length = len(numbers)
+
+        if (length % 2 == 1):
+            median = numbers[length // 2]
+        else:
+            median = (numbers[(length//2-1)] + numbers[length//2]) /2
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
